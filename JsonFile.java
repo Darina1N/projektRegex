16455735a8d3a95bb0952e33faad06fe1630a894
Darina Kolesarova <darina.kolesarova@kosickaakademia.sk>
2021-01-25 19:24:31 +0100

    JSON aj s array

--- JsonFile.java
+++ JsonFile.java
@@ -1,20 +1,24 @@
 package sk.kosickaakademia.kolesarova;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 public class JsonFile {
-    public static void main(String[] args) throws JSONException {
+    public static void main(String[] args) {
 
         JSONObject obj=new JSONObject();
-        JSONArray jsonArray=new JSONArray();
+        JSONArray jsonArray= new JSONArray();
+
+        jsonArray.add("sport");
+        jsonArray.add("tanec");
+        jsonArray.add("kniha");
         
+
         obj.put("age",32);
         obj.put("name","Milan");
-        obj.put("sport","hockey");
+        //obj.put("sport","hockey");
+        obj.put("hobby",jsonArray);
         obj.put("active",true);
-
 
 
         System.out.println(obj);
